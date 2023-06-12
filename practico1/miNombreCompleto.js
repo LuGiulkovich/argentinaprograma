f54b4300450acbb7bbf888b1a25fd2eff6ac0452
LuGiulkovich <134790339+LuGiulkovich@users.noreply.github.com>
2023-06-12 11:33:47 -0300

    Agrego miNombreCompleto.js

---
+++ practico1/miNombreCompleto.js
@@ -0,0 +1,23 @@
+//Defino la variable "nombre" 
+//y la inicializo con mi nombre real "Luciano"
+
+let nombre = "Luciano";
+
+//Luego defino la segunda variable "apellido" 
+//y la inicializo con mi apellido real "Giulkovich"
+
+let apellido = "Giulkovich";
+
+//Declaro la variable "edad"
+//y la inicializo con mi edad "21"
+
+let edad = 21;
+
+//Por último, imprime por consola un mensaje 
+//que respete el siguiente formato:
+//"Hola, [nombre] [apellido]. Tienes [edad] años."
+
+console.log("Hola, %s %s. Tienes %i años.", nombre, apellido, edad)
+
+//Uso la funcion "formatters: %s, $i..."
+//para que sea mas legible mi codigo
