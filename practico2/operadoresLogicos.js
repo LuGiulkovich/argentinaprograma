f5638a2aa51286f593213af97a2b3d887e685012
LuGiulkovich <134790339+LuGiulkovich@users.noreply.github.com>
2023-06-17 10:28:22 -0300

    Agrego operadoresLogicos.js

---
+++ practico2/operadoresLogicos.js
@@ -0,0 +1,26 @@
+//Escribe un programa que solicite al usuario ingresar un número entero.
+//Guardar el programa en un archivo operadoresLogicos.js. 
+
+const readline = require("readline-sync")
+const numero = readline.question('Dime un numero ');
+
+//A continuación, evalúa las siguientes condiciones:
+//- Si el número es positivo y par, muestra por consola el mensaje: "El
+//numero es positivo y par".
+//- Si el número es positivo e impar, muestra por consola el mensaje: "El
+//numero es positivo e impar".
+//- Si el número es negativo, muestra por consola el mensaje: "El numero es
+//negativo".
+//- Si el número es cero, muestra por consola el mensaje: "El numero es
+//cero".
+//NOTA: utilizar los operadores lógicos.
+
+if (numero == 0) {
+    console.log('El numero es cero');
+} else if (numero >= 0 && (numero % 2) === 0) {
+    console.log('El numero es positivo y par');
+} else if (numero >= 0 && (numero % 2) !== 0) {
+    console.log('El numero es positivo e impar'); 
+} else if (numero <= 0) {
+    console.log('El numero es negativo');
+}
