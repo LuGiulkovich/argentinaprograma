07624647e525c168756f1641e08184f28b3a9204
LuGiulkovich <134790339+LuGiulkovich@users.noreply.github.com>
2023-06-17 10:24:41 -0300

    Agrego positivoNegativo.js

---
+++ practico2/positivoNegativo.js
@@ -0,0 +1,10 @@
+const readline = require("readline-sync")
+const numero = readline.question('Dime un numero ');
+
+if (numero > 0) {
+    console.log('El numero es positivo')
+} else if (numero < 0) {
+    console.log('El numero es negativo')
+} else { (numero === 0) 
+    console.log('El numero es 0')
+}
